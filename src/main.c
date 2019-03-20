c2d0b4d19641f5b52b7f17449a5cab073c23fb80
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-03-20 15:44:53 -0500

    Draw path and walls
    Path:
    - och to remember last motion
    - Long checks of och vs ch for drawing line at last spot
    
    Walls:
    - Add checks at each switch-case for motion for if it's empty
    - Create random walls at beginning
      + Changable chance based off surroundings to create islands etc.
    
    Misc:
    - Seed random based off clock not time
    - Spaces around some operators
    - Don't redraw map every time, only the sprite
    - Don't set memory to all zeros and change most of it again right after
    - Set sprite based off motion
    - Use echochar and move

--- src/main.c
+++ src/main.c
@@ -18,26 +18,38 @@
 #include "main.h"
 
 int main (int argc, char * argv[]) {
-  srand(time(NULL));
+  srand(clock());
   int invent_used = 0;
-  int ch = 0;
+  int ch = 0, och = 0;
   int width = 36;
   int height = 18;
   char * base_map = NULL;
-  base_map = malloc(height * width+1);
+  base_map = malloc(height * width + 1);
   if (base_map == NULL) {
     fprintf(stderr, "Ran out of memory\n");
     exit(EXIT_FAILURE);
   }
-  memset(base_map, 0, height * width + 1);
-  memset(base_map, '.', height * width);
+  base_map[height * width] = 0;
+  int chance = 40, uchance = 5, dchance = -10;
+  for (int y = 0; y < height; ++y)
+  	for (int x = 0; x < width; ++x) {
+  		// Default chance is 40 for a wall
+  		// Increases if the nearest are also walls
+  		// Meaning: better chance for large chunks of wall
+  		if (rand() % 100 < chance +
+  		(x > 0 && base_map[y * width + x - 1] == '#' ? uchance : dchance) +
+  		(y > 0 && base_map[y * width - width + x] == '#' ? uchance : dchance))
+  			base_map[y * width + x] = '#';
+  		else base_map[y * width + x] = '.';
+  	}
   char * map = NULL;
-  map = malloc(height * width+1);
-  memset(map, 0, height * width + 1);
+  map = malloc(height * width + 1);
+  map[height * width] = 0;
   memcpy(map, base_map, height * width);
   int x = 3;
   int y = 3;
   int * invent_item_count = NULL;
+  char me = '@';
   invent_item_count = calloc(item_count, sizeof(uint16_t));
   initscr();
   raw();
@@ -45,20 +57,27 @@ int main (int argc, char * argv[]) {
   curs_set(0);
   keypad(stdscr, TRUE);
   noecho();
+  for (int i = 0; i < height; ++i) {
+    mvprintw(i, 0, "%.*s", width, (map + (i * width)));
+  }
   while (ch != 'q') {
-   // clear();
-    switch (ch) {
+    move(y, x);
+  	switch (ch) {
     case KEY_UP:
-      if (y > 0) y--;
+      if (y > 0 && base_map[y * width - width + x] == '.') y--;
+      me = '^';
       break;
     case KEY_DOWN:
-      if (y < height - 1) y++;
+      if (y < height - 1 && base_map[y * width + width + x] == '.') y++;
+      me = 'v';
       break;
     case KEY_LEFT:
-      if (x > 0) x--;
+      if (x > 0 && base_map[y * width + x - 1] == '.') x--;
+      me = '<';
       break;
     case KEY_RIGHT:
-      if (x < width - 1) x++;
+      if (x < width - 1 && base_map[y * width + x + 1] == '.') x++;
+      me = '>';
       break;
     case KEY_A1:
       if (x > 0) x--;
@@ -77,11 +96,73 @@ int main (int argc, char * argv[]) {
       if (y < height - 1) y++;
       break;
     }
-    //memcpy(map, base_map, height * width);
-    for (int i = 0; i < height; ++i) {
-      mvprintw(i, 0, "%.*s\n", width, (map + (i * width)));
+    switch (och) {
+    case KEY_UP:
+    	switch (ch) {
+    	case KEY_UP:
+    	case KEY_DOWN:
+    		echochar(ACS_VLINE);
+    		break;
+    	case KEY_RIGHT:
+    		echochar(ACS_ULCORNER);
+    		break;
+    	case KEY_LEFT:
+    		echochar(ACS_URCORNER);
+    		break;
+    	}
+    	break;
+    case KEY_RIGHT:
+    	switch (ch) {
+    	case KEY_UP:
+    		echochar(ACS_LRCORNER);
+    		break;
+    	case KEY_RIGHT:
+    	case KEY_LEFT:
+    		echochar(ACS_HLINE);
+    		break;
+    	case KEY_DOWN:
+    		echochar(ACS_URCORNER);
+    		break;
+    	}
+    	break;
+    case KEY_LEFT:
+    	switch (ch) {
+    	case KEY_UP:
+    		echochar(ACS_LLCORNER);
+    		break;
+    	case KEY_RIGHT:
+    	case KEY_LEFT:
+    		echochar(ACS_HLINE);
+    		break;
+    	case KEY_DOWN:
+    		echochar(ACS_ULCORNER);
+    		break;
+    	}
+    	break;
+    case KEY_DOWN:
+    	switch (ch) {
+    	case KEY_RIGHT:
+    		echochar(ACS_LLCORNER);
+    		break;
+    	case KEY_LEFT:
+    		echochar(ACS_LRCORNER);
+    		break;
+    	case KEY_DOWN:
+    	case KEY_UP:
+    		echochar(ACS_VLINE);
+    		break;
+    	}
+    	break;
     }
-    mvaddch(y, x, '@');
+    move(y, x);
+    echochar(me);
+    switch (ch) {
+    case KEY_UP:
+    case KEY_RIGHT:
+    case KEY_LEFT:
+    case KEY_DOWN:
+    	och = ch;
+    }
     ch = getch();
     refresh();
   }
