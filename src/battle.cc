c15ef6374d4513eab27a8c5a1e9eeb8c69bb3e44
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-06-07 17:43:28 -0500

    Added exp functions Added Player class - Has a HealthBar, interacts with it - Extends Stats - Better hit and heal functions May consider just merging Stats and Player Add a health bar for you Move actions into a little lambda
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- src/battle.cc
+++ src/battle.cc
@@ -3,6 +3,7 @@
 #include <thread>
 #include <chrono>
 #include <cctype>
+#include <cmath>
 
 #include "correct_curses.h"
 
@@ -19,9 +20,51 @@ namespace {
 		std::this_thread::sleep_for(x);
 		return flushinp();
 	}
+
+	int expLevel (int level) {
+		if (level == 1) return 0;
+		double result = 20 * std::pow(level, 1.7);
+		double tens = std::pow(10, std::ceil(std::log10(result) - 3));
+		return std::floor(20 * std::pow(level, 1.7) / tens) * tens;
+	}
+
+	int cumLevel (int level) {
+		int sum = 0;
+		for (int i = 1; i <= level; ++i) sum += expLevel(i);
+		return sum;
+	}
+
+	class Player : public game::Stats {
+	public:
+		game::HealthBar healthbar;
+
+		Player (int h, int a, int d, int s, int b_h, int y, int x):
+		Stats(h, a, d, s),
+		healthbar(y, x, b_h) {};
+
+		// Update health bar based off health stat
+		void updateBar () {
+			healthbar.update(health_ * healthbar.max_health() / max_health_);
+		}
+
+		int hit (Player &other) const noexcept {
+			other.health_ -= attack_ - other.defense_ + 1;
+			other.updateBar();
+			return attack_ - other.defense_ + 1;
+		}
+
+		int heal (int amt) noexcept {
+			if (amt < 0) return 0;
+			int oh = health_;
+			health_ = health_ + amt < max_health_ ? health_ + amt : max_health_;
+			updateBar();
+			return health_ - oh;
+		}
+	}; // class Player
 } // namespace
 
 int main () {
+	using namespace std::chrono_literals;
 	initscr();
 #ifdef HAVE_CURSES_COLOR
 	if (has_colors() == TRUE) start_color();
@@ -30,53 +73,50 @@ int main () {
 	noecho();
 	scrollok(stdscr, FALSE);
 	curs_set(0);
-	timeout(50);
+	// timeout(50);
 
-	game::HealthBar theirs(0, 9, 20);
-
-	game::Stats you(20, 5, 8, 1), them(20, 10, 4, 2);
+	Player you(20, 5, 8, 1, 20, 3, 9), them(20, 10, 4, 2, 20, 0, 9);
 
 	int ch = 0;
 	mvaddstr(0, 0, "them:");
 	mvaddstr(1, 0, "Health");
-	mvprintw(2, 0, "you:");
-	mvprintw(3, 0, "Health - %d", you.health());
-	mvprintw(4, 0, "Attack - %d", you.attack());
-	mvprintw(5, 0, "Defense - %d", you.defense());
+	mvprintw(3, 0, "you:");
+	mvprintw(4, 0, "Health", you.health());
+	mvprintw(6, 0, "Attack - %d", you.attack());
+	mvprintw(7, 0, "Defense - %d", you.defense());
 	refresh();
-	theirs.disp();
-	bool them_to_attack = false;
-	while (ch != 'q' && ch != CTRL('c') && you.health() > 0 && them.health() > 0) {
+	them.healthbar.disp();
+	you.healthbar.disp();
+	auto your_action = [&] {
 		switch (ch) {
 			case 'a':
-				you.attack(them);
-				theirs.update(them.health() * them.max_health() / 20);
-				if (them.health() > 0) them_to_attack = true;
-				wait_for(std::chrono::milliseconds(500));
+				you.hit(them);
 				break;
 			case 'h':
 				you.heal(5);
-				mvprintw(3, 9, "%d ", you.health());
-				refresh();
-				them_to_attack = true;
-				wait_for(std::chrono::milliseconds(500));
 				break;
 		}
-		if (them_to_attack) {
-			them_to_attack = false;
-			them.attack(you);
-			mvprintw(3, 9, "%d ", you.health());
-			refresh();
+	};
+	while (ch != 'q' && ch != CTRL('c') && you.health() > 0 && them.health() > 0) {
+		ch = tolower(getch()); if (ch == 'q' || ch == CTRL('c')) break;
+		if (you.speed() > them.speed()) {
+			your_action();
+			wait_for(500ms);
+			if (them.health() > 0) them.hit(you);
+		} else {
+			them.hit(you);
+			wait_for(500ms);
+			if (you.health() > 0) your_action();
 		}
-		ch = tolower(getch());
+		wait_for(350ms);
 	}
 	timeout(-1);
 	if (you.health() <= 0) {
-		mvaddstr(6, 0, "You died");
+		mvaddstr(8, 0, "You died");
 		refresh();
 		getch();
 	} else if (them.health() <= 0) {
-		mvaddstr(6, 0, "You killed them");
+		mvaddstr(8, 0, "You killed them");
 		refresh();
 		getch();
 	}
