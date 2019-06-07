33b2afac16e481e91cc047d64661fe751b5d108f
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-06-07 16:43:03 -0500

    Changed HealthBar
    Added access function for max_health
    Changed max_health to max_health_
    Changed cur_health to cur_health_
    Changed constructor to make a derived window
    Changed/Added a few comments
    Default hascolor to false
    Use tabs
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- src/health_bar.cc
+++ src/health_bar.cc
@@ -1,14 +1,14 @@
 #include "health_bar.h"
 
 namespace game {
-	// Update new health and display (assuming no changes)
+	// Update new health and display
 	void HealthBar::update (int h) {
-		if (h < 0 || h > max_health) return;
+		if (h < 0 || h > max_health_) return;
 #ifdef HAVE_CURSES_COLOR
 		if (has_color) {
-			if (h <= max_health / 10)
+			if (h <= max_health_ / 10)
 				wattron(win, COLOR_PAIR(pairs[2]));
-			else if (h <= max_health / 2)
+			else if (h <= max_health_ / 2)
 				wattron(win, COLOR_PAIR(pairs[1]));
 			else
 				wattron(win, COLOR_PAIR(pairs[0]));
@@ -20,31 +20,31 @@ namespace game {
 #else
 			mvwhline(win, 1, 1, '#', h);
 #endif
-		mvwhline(win, 1, h + 1, ' ', max_health - h);
-		cur_health = h;
+		mvwhline(win, 1, h + 1, ' ', max_health_ - h);
+		cur_health_ = h;
 		wrefresh(win);
 	}
 
-	// Assume changes and display
+	// Redraw
 	void HealthBar::disp () {
 		wborder(win, border_ch[0], border_ch[1], border_ch[2], border_ch[3], border_ch[4], border_ch[5], border_ch[6], border_ch[7]);
 #ifdef HAVE_CURSES_COLOR
 		if (has_color) {
-			if (cur_health <= max_health / 10)
+			if (cur_health_ <= max_health_ / 10)
 				wattron(win, COLOR_PAIR(pairs[2]));
-			else if (cur_health <= max_health / 2)
+			else if (cur_health_ <= max_health_ / 2)
 				wattron(win, COLOR_PAIR(pairs[1]));
 			else
 				wattron(win, COLOR_PAIR(pairs[0]));
-			mvwhline(win, 1, 1, '#', cur_health);
+			mvwhline(win, 1, 1, '#', cur_health_);
 			wattron(win, COLOR_PAIR(0));
 		} else {
-			mvwhline(win, 1, 1, '#', cur_health);
+			mvwhline(win, 1, 1, '#', cur_health_);
 		}
 #else
-		mvwhline(win, 1, 1, '#', cur_health);
+		mvwhline(win, 1, 1, '#', cur_health_);
 #endif
-		mvwhline(win, 1, cur_health + 1, ' ', max_health - cur_health);
+		mvwhline(win, 1, cur_health_ + 1, ' ', max_health_ - cur_health_);
 		wrefresh(win);
 	}
 }
--- src/health_bar.h
+++ src/health_bar.h
@@ -4,38 +4,46 @@
 #define GAME_HEALTH_BAR_H_
 
 namespace game {
-  class HealthBar {
-	  WINDOW *win;
-	  int max_health;
-	  int cur_health;
-	  int border_ch[8];
-	  bool has_color;
-	  short pairs[3];
-  public:
-    HealthBar (int ul_y, int ul_x, int max,
-	  int b0 = 0, int b1 = 0, int b2 = 0, int b3 = 0, int b4 = 0, int b5 = 0, int b6 = 0, int b7 = 0):
-	  max_health(max), cur_health(max), border_ch{b0, b1, b2, b3, b4, b5, b6, b7} {
-		  win = newwin(3, max + 2, ul_y, ul_x);
-		  has_color = false;
+	class HealthBar {
+		WINDOW *win;
+		int max_health_;
+		int cur_health_;
+		int border_ch[8];
+		bool has_color;
+		short pairs[3];
+	public:
+
+		decltype(max_health_) max_health () const { return max_health_; }
+
+		HealthBar (int ul_y, int ul_x, int max, WINDOW *supwin = stdscr,
+		int b0 = 0, int b1 = 0, int b2 = 0, int b3 = 0,
+		int b4 = 0, int b5 = 0, int b6 = 0, int b7 = 0):
+		max_health_(max), cur_health_(max), border_ch{b0, b1, b2, b3, b4, b5, b6, b7} {
+			win = supwin == stdscr ? newwin(3, max + 2, ul_y, ul_x) :
+			derwin(supwin, 3, max + 2, ul_y, ul_x);
+			has_color = false;
 #ifdef HAVE_CURSES_COLOR
-		  // We assume color is started
-		  if (has_colors() == TRUE) {
-			  has_color = true;
-			  init_pair(40, COLOR_GREEN, COLOR_BLACK); pairs[0] = 40;
-			  init_pair(41, COLOR_YELLOW, COLOR_BLACK); pairs[1] = 41;
-			  init_pair(42, COLOR_RED, COLOR_BLACK); pairs[2] = 42;
-		  }
+			// We assume color is started
+			if (has_colors() == TRUE) {
+				has_color = true;
+				init_pair(40, COLOR_GREEN, COLOR_BLACK); pairs[0] = 40;
+				init_pair(41, COLOR_YELLOW, COLOR_BLACK); pairs[1] = 41;
+				init_pair(42, COLOR_RED, COLOR_BLACK); pairs[2] = 42;
+			}
 #endif
-	  }
+		}
 
-    void update (int h);
-    void disp ();
+		// Update health & display
+		void update (int h);
 
- 	  ~HealthBar () {
-		  delwin(win);
-	  }
+		// Redraw
+		void disp ();
 
-  };
+		 ~HealthBar () {
+			delwin(win);
+		}
+
+	};
 }
 
 #endif // GAME_HEALTH_BAR_H_
