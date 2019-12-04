65f04b5b70e1f0a8f6e8e4e661b4822c444e31a5
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-12-03 18:25:14 -0600

    Corrected issue in CheckVaArgs.cmake

--- cmake/CheckVaArgs.cmake
+++ cmake/CheckVaArgs.cmake
@@ -43,7 +43,8 @@ check_c_source_compiles(NONE_CODE NONE_WORKS)
 check_c_source_compiles(OPT_CODE OPT_WORKS)
 check_c_source_compiles(CAT_CODE CAT_WORKS)
 
-set(VA_ARGS_NONE NONE_WORKS PARENT_SCOPE)
-set(VA_ARGS_OPT OPT_WORKS PARENT_SCOPE)
-set(VA_ARGS_CAT CAT_WORKS PARENT_SCOPE)
+set(VA_ARGS_NONE "${NONE_WORKS}" PARENT_SCOPE)
+set(VA_ARGS_OPT "${OPT_WORKS}" PARENT_SCOPE)
+set(VA_ARGS_CAT "${CAT_WORKS}" PARENT_SCOPE)
+
 endfunction(check_va_args_macro)
