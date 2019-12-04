3f3d7488c7a20674cad7a21b2296d728d1c52336
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-12-03 23:44:50 -0600

    Essentially I'm dumb and did CMake variables wrong again.

--- cmake/CheckVaArgs.cmake
+++ cmake/CheckVaArgs.cmake
@@ -39,9 +39,9 @@ int main () {
 }"
 )
 
-check_c_source_compiles(NONE_CODE NONE_WORKS)
-check_c_source_compiles(OPT_CODE OPT_WORKS)
-check_c_source_compiles(CAT_CODE CAT_WORKS)
+check_c_source_compiles("${NONE_CODE}" NONE_WORKS)
+check_c_source_compiles("${OPT_CODE}" OPT_WORKS)
+check_c_source_compiles("${CAT_CODE}" CAT_WORKS)
 
 set(VA_ARGS_NONE "${NONE_WORKS}" PARENT_SCOPE)
 set(VA_ARGS_OPT "${OPT_WORKS}" PARENT_SCOPE)
