2a53a267d79d3d0f5e21e4ec87f61c27327bb9ba
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-12-26 22:52:53 -0600

    Install GNU Make for windows
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- travis-windows-setup.sh
+++ travis-windows-setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 if [ "${TRAVIS_OS_NAME}" = "windows" ]; then
+	choco install make
 	wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.1.tar.gz
 	tar zvxf ncurses-6.1.tar.gz
 	cd ncurses-6.1
