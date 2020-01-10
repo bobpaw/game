52ad215eddc903edf9c03ec24428457d985e0ed0
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-01-10 01:07:57 -0600

    Use files in same directory as script
    Move jq script into shell script
    Use a heredoc instead of sed
    Add ARGS where it's needed in CMakeLists
    Add spaces to itemlist.h (via jq script)
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- src/data/itemlist.h
+++ src/data/itemlist.h
@@ -7,13 +7,13 @@
 
 namespace game {
 	const std::array<item,7> item_list = {
-		item(0,weapon,fists,"Unarmed",1),
-		item(1,armor,all,"Bare",0),
-		item(2,weapon,onehand,"Dagger",2),
-		item(3,armor,chest,"Tunic",1),
-		item(4,hold,hp,"Level 1 Potion",1),
-		item(5,armor,shield,"Leather Shield",2),
-		item(6,weapon,onehand,"Short Sword",3)
+		item(0, weapon, fists, "Unarmed", 1),
+		item(1, armor, all, "Bare", 0),
+		item(2, weapon, onehand, "Dagger", 2),
+		item(3, armor, chest, "Tunic", 1),
+		item(4, hold, hp, "Level 1 Potion", 1),
+		item(5, armor, shield, "Leather Shield", 2),
+		item(6, weapon, onehand, "Short Sword", 3)
 	};
 
 } // namespace game
--- src/data/items.sh
+++ src/data/items.sh
@@ -17,11 +17,13 @@ which jq > /dev/null || exit 1
 which mawk > /dev/null || alias mawk=awk
 
 # Get an item count
-item_count="$(jq 'length' items.json)"
+item_count="$(jq 'length' $(dirname $0)/items.json)"
 
-items="$(jq -r -f items.jq items.json | mawk -v item_count=$item_count 'NR != item_count {print($0 ",")} NR == item_count {print}')"
+items=$(cat <<"EOF" | jq -r -f /dev/stdin $(dirname $0)/items.json | mawk -v item_count=$item_count 'NR != item_count {print($0 ",")} NR == item_count {print}')
+.[] | {class: .class | rtrimstr("able") | ascii_downcase, type: .type | ascii_downcase | sub("one-handed"; "onehand"), name, power, id} | "\t\titem(\(.id), \(.class), \(.type), \"\(.name)\", \(.power))"
+EOF
 
-cat > $(echo "$*" | grep -qF -- '--build' && echo itemlist.h || echo /dev/stdout) <<EOF
+cat > $(echo "$*" | grep -qF -- '--build' && echo $(dirname $0)/itemlist.h || echo /dev/stdout) <<EOF
 #include <array>
 
 #include "item.h"
