d856fe09b25ae71e25e4201f5ab9a1603e6ffbc8
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-01-10 10:51:45 -0600

    Put heredoc inside command substitution
    Quote EOF using single quotes
    Add quotes around command substitution
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- src/data/items.sh
+++ src/data/items.sh
@@ -19,9 +19,10 @@ which mawk > /dev/null || alias mawk=awk
 # Get an item count
 item_count="$(jq 'length' $(dirname $0)/items.json)"
 
-items=$(cat <<"EOF" | jq -r -f /dev/stdin $(dirname $0)/items.json | mawk -v item_count=$item_count 'NR != item_count {print($0 ",")} NR == item_count {print}')
+items="$(cat <<'EOF' | jq -r -f /dev/stdin $(dirname $0)/items.json | mawk -v item_count=$item_count 'NR != item_count {print($0 ",")} NR == item_count {print}'
 .[] | {class: .class | rtrimstr("able") | ascii_downcase, type: .type | ascii_downcase | sub("one-handed"; "onehand"), name, power, id} | "\t\titem(\(.id), \(.class), \(.type), \"\(.name)\", \(.power))"
 EOF
+)"
 
 cat > $(echo "$*" | grep -qF -- '--build' && echo $(dirname $0)/itemlist.h || echo /dev/stdout) <<EOF
 #include <array>
